87b5aece4b88c3a91997fca4e62540bd31f64249
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-05-29 16:17:28 +0530

    feat added delete btn

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const sizeEl = document.getElementById("size");
 const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
-let size = 30;
+let size = 5;
 let x = undefined;
 let y = undefined;
 let isPressed = false;
@@ -28,17 +29,26 @@ canvas.addEventListener("mousemove", (e) => {
   if (isPressed) {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
+    drawCircle(x,y);
     drawLine(x, y, x2, y2);
     x = x2;
     y = y2;
   }
 });
 
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
 function drawLine(x1, y1, x2, y2){
     ctx.beginPath();
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
     ctx.strokeStyle = color;
+    ctx.lineWidth = size*2;
     ctx.stroke();
 }
 
@@ -65,6 +75,10 @@ decreaseBtn.addEventListener("click", () => {
 colorEl.addEventListener('change', (e) => {
     color = e.target.value;
 })
+
+clearEl.addEventListener('click', () => {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+});
 
 function updateSizeOnScreen(){
     sizeEl.innerText = size;
