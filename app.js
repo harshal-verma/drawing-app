0efca65b39ca50daba6599dbe95074accec84443
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-05-29 14:21:18 +0530

    feat: added draw line

--- app.js
+++ app.js
@@ -1,35 +1,72 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let size = 10;
-let x = 50;
-let y = 50;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+
+let size = 30;
+let x = undefined;
+let y = undefined;
 let isPressed = false;
 
-canvas.addEventListener('mousedown' , () => {
-    isPressed = true;
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+
 });
 
-canvas.addEventListener('mouseup', () => {
-    isPressed = false;
-}); 
-
-canvas.addEventListener('mousemove', (e) => {
-    if(isPressed){
-    const x = e.offsetX;
-    const y = e.offsetY;
-    drawCircle(x,y);
-    }
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
 });
 
-function drawCircle(x, y){
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
+});
+
+function drawLine(x1, y1, x2, y2){
     ctx.beginPath();
-    ctx.arc(x,y,size,0,Math.PI*2);
-    ctx.fill();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.strokeStyle = color;
+    ctx.stroke();
 }
 
-// drawCircle(50,50);
+increaseBtn.addEventListener("click", () => {
+    size += 5;
 
-// function draw(){
-//     drawCircle(50,50);
-//     requestAnimationFrame(draw);
-// }
+    if (size > 50) {
+        size = 50;
+    }
+
+    updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+    size -= 5;
+
+    if (size < 5) {
+        size = 5;
+    }
+
+    updateSizeOnScreen();
+});
+
+colorEl.addEventListener('change', (e) => {
+    color = e.target.value;
+})
+
+function updateSizeOnScreen(){
+    sizeEl.innerText = size;
+}
+
